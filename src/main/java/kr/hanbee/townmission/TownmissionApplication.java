c5556c8e8fa1fbe6f5f762d7d749e67849211688
hanbee <sonhanbi1002@gmail.com>
2022-02-19 14:58:06 +0900

    init commit

---
+++ src/main/java/kr/hanbee/townmission/TownmissionApplication.java
@@ -0,0 +1,13 @@
+package kr.hanbee.townmission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TownmissionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TownmissionApplication.class, args);
+    }
+
+}
