3596d8bcf2a2a632db92bae88d5878c2f279c718
hanbee <sonhanbi1002@gmail.com>
2022-02-19 15:44:51 +0900

    mission repository jpa 설정

--- src/main/java/kr/hanbee/townmission/repository/MissionRepository.java
+++ src/main/java/kr/hanbee/townmission/repository/MissionRepository.java
@@ -1,4 +1,7 @@
 package kr.hanbee.townmission.repository;
 
-public interface MissionRepository {
+import kr.hanbee.townmission.domain.Mission;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissionRepository extends JpaRepository<Mission, Long> {
 }
